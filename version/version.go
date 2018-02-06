532f967d77ac25bbb56244ea9666d38c58885827
Zhanchun Zhang <zzchun@zju.edu.cn>
2018-02-06 14:59:17 +0800

    fix: change build time to a fixed value and add commit id
    Signed-off-by: Zhanchun Zhang <zzchun@zju.edu.cn>

--- version/version.go
+++ version/version.go
@@ -6,11 +6,13 @@ import "time"
 const Version = "0.1.0"
 
 // BuildTime is the time when this binary of daemon is built
-// FIXME: this is dynamical. We need a fixed build time.
-var BuildTime = time.Now().Format(time.RFC3339Nano)
+var BuildTime = time.Date(2018, 1, 19, 8, 0, 0, 0, time.Local).Format(time.RFC3339Nano)
 
 // APIVersion means the api version daemon serves
 var APIVersion = "1.24"
 
 // GOVersion is the go version to build Pouch
 var GOVersion = "go1.9.1"
+
+// GitCommit is the commit id to build Pouch
+var GitCommit = "6be2080cd9837e9b8a0039c2d21521bb00a30c84"
